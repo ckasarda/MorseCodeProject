f5013cbcc4cb557d013b515406bdcdb8349fa5eb
Connor L Kasarda <47127368+ckasarda@users.noreply.github.com>
2019-04-29 17:49:59 -0600

    Update MorseProjectCode.cpp
    Now requires input from user!

--- MorseProjectCode.cpp
+++ MorseProjectCode.cpp
@@ -6,11 +6,19 @@
 //Coder Reviewer: Matt Sharp
 
 #include "pch.h"
-#include <iostream>
+#include <iostream> //Allows for cin and cout
 #include <string> //Allows increased functionality with strings
 #include <fstream> //Allows for input and output with text files
 
 using namespace std;
+
+char lSymbol = '.';//Symbol of the morse code, representing the left branching in the decode tree
+char rSymbol = '-';//Symbol of the morse code, representing the right branching in the decode tree
+
+bool test_file(string file) {//Tests to see if the file can be opened, returning a boolean value based on result
+	ifstream input(file);
+	return input.good();
+}
 
 struct Node {//Creates the nodes that will build up the decode binary tree
 	char letter = ' ';
@@ -30,18 +38,73 @@ int main()
 	//Root of decode tree with no value
 	Node * DecodeTree = new Node;
 
+	char langc = ' ';//stores choice of the user for Morse or English file to be opened
+	char again = ' ';//stores the answer for whether the user wants to use the program again or not
+	string mtxt = "";//stores the file name for the Morse code text file
+	string etxt = "";//stores the file name for the English code text file
+
 	//Constructs the morse code keys into the decode binary tree
 	decodeTreeConstructor("MorseTable.txt", DecodeTree);
 
-	//English to Morse example
-	E2M("MorseTable.txt", "E2MTest3.txt");
+	//Ask the user if they want to open a Morse file or an English file, repeating as many times as the user wants to run the program
+	do {
 
-	//Separates E2M example from M2E example
-	cout << endl << endl;
+		//Asks the user whether they would like to open a Morse text file or an English text file
+		cout << "\nWould you like to open a Morse code file or an English file? (m for Morse, e for English): ";
+		cin >> langc;
 
-	//Morse to English example
-	M2E(DecodeTree, "M2ETest3.txt");
+		//Loops until user enters valid input
+		while (langc != 'm' && langc != 'M' && langc != 'e' && langc != 'E') {
 
+			cout << "\nInvalid Entry!" << endl;//Displays error message
+			cout << "\nWould you like to open a Morse code file or an English file? (m for Morse, e for English): ";
+			cin >> langc;
+
+		}
+
+		//Depending on user input, checks if text file is Morse or English
+		if (langc == 'm' || langc == 'M') {
+			cout << "\nType in the name of the Morse file (1. Make sure it is a Morse file! 2. Make sure it is in the same location as the program!)" << endl;
+			cout << "Example: SecretMorse.txt" << endl;
+			cout << "Enter file name here: ";
+			cin >> mtxt;
+			if (test_file(mtxt))//Tests if the file exists
+				M2E(DecodeTree, mtxt);
+			else {
+				cout << "\nNot found! Try again!" << endl;
+				while (!test_file(mtxt)) {
+					cout << "\nType in the name of the Morse file (1. Make sure it is a Morse file! 2. Make sure it is in the same location as the program!)" << endl;
+					cout << "Example: SecretMorse.txt" << endl;
+					cout << "Enter file name here: ";
+					cin >> mtxt;
+				}
+				M2E(DecodeTree, mtxt);
+			}
+
+		}
+		else {
+			cout << "\nType in the name of the English file (1. Make sure it is an English file! 2. Make sure it is in the same location as the program!):" << endl;
+			cout << "Example: SecretEnglish.txt" << endl;
+			cout << "Enter file name here: ";
+			cin >> etxt;
+			if (test_file(etxt))//Tests if the file exists
+				E2M("MorseTable.txt", etxt);
+			else {
+				cout << "\nNot found! Try again!" << endl;
+				while (!test_file(etxt)) {
+					cout << "\nType in the name of the Morse file (1. Make sure it is a Morse file! 2. Make sure it is in the same location as the program!)" << endl;
+					cout << "Example: SecretMorse.txt" << endl;
+					cout << "Enter file name here: ";
+					cin >> etxt;
+				}
+				E2M("MorseTable.txt", etxt);
+			}
+		}
+
+		cout << "\n\nWould you like to run the program again? (Please enter a y or Y to run again, press anything else otherwise): ";
+		cin >> again;
+
+	} while (again == 'y' || again == 'Y');//Loop Invariant: langc is always going to be m, for Morse, or e, for English, and DecodeTree is always the same binary tree stored with Morse code translation information
 }
 
 void decodeTreeConstructor(string filename, Node * Parent) {
@@ -54,23 +117,29 @@ void decodeTreeConstructor(string filename, Node * Parent) {
 	//Open file used for input
 	ifstream morsedata;
 	morsedata.open(filename);
-	
+
 	//Insert file data into decode tree
 	if (morsedata.is_open()) {
+
+		cout << "Commence Decode Binary Tree construction!\n" << endl;
 
 		//Invariant: letter always has first letter of line, or letter to be represented, and morse has rest of same line from position 2, the morse string equivalent
 		while (getline(morsedata, line)) {
 
 			//Store letter to be inserted into decode binary tree
 			letter = line.front();
+			cout << "Added character: " << letter << endl;
 
 			//Store morse code equivalent into string
 			morse = line.substr(2);
+			cout << "Equivalent morse: " << morse << endl;
 
 			//Add letter into proper location of decode binary tree
 			addLetter(letter, morse, Parent);
 
 		}
+
+		cout << "\nDecode Binary Tree construction complete!" << endl;
 
 	}
 
@@ -82,40 +151,40 @@ void decodeTreeConstructor(string filename, Node * Parent) {
 //Adds letter to the correct node in the decode tree
 void addLetter(char letter_c, string morse, Node * root) {
 
-		if (morse.size() == 0) {//End of the morse code string found,
-								//place the letter value at current node
+	if (morse.size() == 0) {//End of the morse code string found,
+							//place the letter value at current node
 
-			root->letter = letter_c;
+		root->letter = letter_c;
 
-		}
-		else if (morse[0] == '.') {//If a dot is encountered, 
-								   //moves to the node's left child and
-								   //calls the function recursively
+	}
+	else if (morse[0] == lSymbol) {//If a dot is encountered, 
+							   //moves to the node's left child and
+							   //calls the function recursively
 
-			if (root->leftNode == NULL) {//Creates an empty left child node
-										 //incase one does not already exist there
+		if (root->leftNode == NULL) {//Creates an empty left child node
+									 //incase one does not already exist there
 
-				root->leftNode = new Node;
-
-			}
-
-			addLetter(letter_c, morse.substr(1), root->leftNode);
+			root->leftNode = new Node;
 
 		}
-		else if (morse[0] == '-') {//If a dash is encountered, 
-								   //moves to the node's right child and
-								   //calls the function recursively
 
-			if (root->rightNode == NULL) {//Creates an empty right child node
-										  //incase one does not already exist there
+		addLetter(letter_c, morse.substr(1), root->leftNode);
 
-				root->rightNode = new Node;
+	}
+	else if (morse[0] == rSymbol) {//If a dash is encountered, 
+							   //moves to the node's right child and
+							   //calls the function recursively
 
-			}
+		if (root->rightNode == NULL) {//Creates an empty right child node
+									  //incase one does not already exist there
 
-			addLetter(letter_c, morse.substr(1), root->rightNode);
+			root->rightNode = new Node;
 
 		}
+
+		addLetter(letter_c, morse.substr(1), root->rightNode);
+
+	}
 
 }
 
@@ -124,14 +193,14 @@ void findLetter(string morse, Node * root) {
 
 	char letter_c;
 	string morse_code = morse;
-	
+
 	if (morse.size() == 0) {//Found end of the morse code string,
 							//displays letter found at current node
 		letter_c = root->letter;
 		cout << letter_c;
 
 	}
-	else if (morse[0] == '.') {//If a dot encountered, 
+	else if (morse[0] == lSymbol) {//If a dot encountered, 
 							   //call function recursivly on left child node
 
 		if (root->leftNode == NULL) {//If node does not exist along path to value,
@@ -148,7 +217,7 @@ void findLetter(string morse, Node * root) {
 		}
 
 	}
-	else if (morse[0] == '-') {//If a dash encountered, 
+	else if (morse[0] == rSymbol) {//If a dash encountered, 
 							   //call function recursivly on right child node
 
 		if (root->rightNode == NULL) {//If node does not exist along path to value,
@@ -188,7 +257,7 @@ void E2M(string morsetable, string morsefile) {
 
 	ifstream morse_table;
 	morse_table.open(morsetable);
-	
+
 	//Finds length of morse table
 	morse_table.seekg(0, morse_table.end);
 	streamoff table_length = morse_table.tellg();
@@ -198,12 +267,12 @@ void E2M(string morsetable, string morsefile) {
 	char * table_buffer = new char[int(table_length)];
 
 	//Read morse code table text as block
-	morse_table.read(table_buffer,table_length);
+	morse_table.read(table_buffer, table_length);
 
 	//Prints english message as morse equivalent
 	//Invariant: i is always less than character length of text in message file
 	for (int i = 0; i < message_length; i++) {
-		
+
 		//Invariant: j is always less than character length of text in morse table file
 		for (int j = 0; j < table_length; j++) {
 
@@ -213,13 +282,13 @@ void E2M(string morsetable, string morsefile) {
 				int z = 2;
 
 				//Invariant: morse_string is either part of or the entirety of the morse string equivalent of the letter so far
-				while (table_buffer[j + z] == '.' || table_buffer[j + z] == '-') {//Tanslates letter to morse equivalent
+				while (table_buffer[j + z] == lSymbol || table_buffer[j + z] == rSymbol) {//Tanslates letter to morse equivalent
 
 					//Adds dot or dash to morse string variable according to morse table file
-					if (table_buffer[j + z] == '.')
-						morse_string += ".";
-					else if (table_buffer[j + z] == '-')
-						morse_string += "-";
+					if (table_buffer[j + z] == lSymbol)
+						morse_string += lSymbol;
+					else if (table_buffer[j + z] == rSymbol)
+						morse_string += rSymbol;
 
 					//Increments variable z
 					z++;
@@ -274,10 +343,10 @@ void M2E(Node * root, string engfile) {
 	//Invariant: morse is always either part of or the entirety of the morse code string equivalent to be decoded to english
 	for (int i = 0; i < eng_length; i++) {//Siphens through the entire buffer of the English word file
 
-		if (eng_buffer[i] == '.')//Adds a dot to the morse code string to be decoded to English
-			morse += ".";
-		else if (eng_buffer[i] == '-')//Adds a dash to the morse code string to be decoded to English
-			morse += "-";
+		if (eng_buffer[i] == lSymbol)//Adds a dot to the morse code string to be decoded to English
+			morse += lSymbol;
+		else if (eng_buffer[i] == rSymbol)//Adds a dash to the morse code string to be decoded to English
+			morse += rSymbol;
 		else if (eng_buffer[i] == ' ' && eng_buffer[i + 1] == ' ' && eng_buffer[i + 2] == ' ') {//If three spaces, indicates a space and beginning of new word.
 																								//Finds the letter from morse code string then prints a space
 																								//before reseting the morse code string
